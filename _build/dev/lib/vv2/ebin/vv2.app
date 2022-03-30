afa891f05a5efc3e21943232028f6aaed6d443ae
mhoffnn <matheushofmar@hotmail.com>
2022-03-30 10:40:48 -0300

    AS5

---
+++ _build/dev/lib/vv2/ebin/vv2.app
@@ -0,0 +1,6 @@
+{application,vv2,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"vv2"},
+              {modules,['Elixir.Vv2']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
---
+++ lib/vv2.ex
@@ -0,0 +1,33 @@
+defmodule Vv2 do
+  def deducao_irpf_alternativo(base) do
+    cond do
+      base >= 0.00 and base < 1903.98 ->
+        0.00
+
+      base >= 1903.98 and base < 2826.65 ->
+        142.80
+
+      base >= 2826.65 and base < 3751.06 ->
+        354.80
+
+      base >= 3751.06 and base < 4664.68 ->
+        636.13
+
+      base >= 4664.68 and is_number(base) ->
+        869.36
+
+      true ->
+        :valor_invalido
+    end
+  end
+
+  def deducao_irpf(base) when base < 0 or not is_number(base) do
+    :valor_invalido
+  end
+
+  def deducao_irpf(base) when base < 1900.90, do: 0.00
+  def deducao_irpf(base) when base >= 1900.90 and base < 2826.65, do: 142.80
+  def deducao_irpf(base) when base >= 2826.65 and base < 3751.06, do: 354.80
+  def deducao_irpf(base) when base >= 3751.06 and base < 4664.68, do: 636.13
+  def deducao_irpf(base) when base >= 4664.68, do: 869.36
+end
