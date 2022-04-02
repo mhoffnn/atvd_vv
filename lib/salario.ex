1201f010b808286c750a2df9ea6576c6fdcde6c0
mhoffnn <matheushofmar@hotmail.com>
2022-04-02 11:34:14 -0300

    created module Salario, using method TDD, to complete the AS8

---
+++ lib/salario.ex
@@ -0,0 +1,65 @@
+defmodule Salario do
+  def calc(), do: :do
+
+  def calc(user) do
+    user
+    |> is_valid?()
+  end
+
+  def is_valid?(
+        user = %{
+          name: name,
+          email: email,
+          base_salary: base_salary,
+          office: office
+        }
+      ) do
+    name
+    |> is_name?()
+    |> is_email?(email)
+    |> is_salary?(base_salary)
+    |> is_office?(office)
+    |> rebase_by_office(user)
+  end
+
+  def is_valid?(_user), do: :missing_information
+
+  def is_name?(name), do: String.length(name) > 2
+
+  def is_email?(bool, _email) when bool == false, do: bool
+
+  def is_email?(_bool, email) do
+    String.contains?(email, "@") and String.contains?(email, ".com")
+  end
+
+  def is_salary?(bool, _base_salary) when bool == false, do: bool
+  def is_salary?(_bool, base_salary), do: is_number(base_salary) and base_salary > 0
+
+  def is_office?(bool, _office) when bool == false, do: bool
+
+  def is_office?(_bool, office) do
+    is_atom(office) and (office == :dev or office == :dba or office == :tst or office == :ger)
+  end
+
+  def rebase_by_office(bool, _user) when bool == false, do: :informations_invalid
+
+  def rebase_by_office(_bool, user) do
+    rebase(user.base_salary, user.office)
+  end
+
+  def rebase(base, :dev) when base < 3000, do: Float.floor(base - percent(base, 10), 2)
+  def rebase(base, :dev), do: Float.floor(base - percent(base, 20), 2)
+
+  def rebase(base, :dba) when base < 2000, do: Float.floor(base - percent(base, 15), 2)
+  def rebase(base, :dba), do: Float.floor(base - percent(base, 25), 2)
+
+  def rebase(base, :tst) when base < 2000, do: Float.floor(base - percent(base, 15), 2)
+  def rebase(base, :tst), do: Float.floor(base - percent(base, 25), 2)
+
+  def rebase(base, :ger) when base < 5000, do: Float.floor(base - percent(base, 20), 2)
+  def rebase(base, :ger), do: Float.floor(base - percent(base, 30), 2)
+
+  def percent(base, percent) do
+    percent * base / 100
+  end
+end
---
+++ test/salario_test.exs
@@ -0,0 +1,164 @@
+defmodule SalarioTest do
+  use ExUnit.Case
+  doctest Salario
+
+  test "refute test calc" do
+    refute Salario.calc() == :doing
+  end
+
+  test "user don't have name informations" do
+    assert %{
+             email: "matheus@mail.com",
+             base_salary: 1200,
+             office: :dev
+           }
+           |> Salario.calc() == :missing_information
+  end
+
+  test "user don't have email informations" do
+    assert %{
+             name: "Matheus",
+             base_salary: 1200,
+             office: :dev
+           }
+           |> Salario.calc() == :missing_information
+  end
+
+  test "user don't have base salary informations" do
+    assert %{
+             name: "Matheus",
+             email: "matheus@mail.com",
+             office: :dev
+           }
+           |> Salario.calc() == :missing_information
+  end
+
+  test "user don't have office informations" do
+    assert %{
+             name: "Matheus",
+             email: "matheus@mail.com",
+             base_salary: 1200
+           }
+           |> Salario.calc() == :missing_information
+  end
+
+  test "user have name invalid" do
+    assert %{
+             name: "Mt",
+             email: "matheus@mail.com",
+             base_salary: 1200,
+             office: :dev
+           }
+           |> Salario.calc() == :informations_invalid
+  end
+
+  test "user have email invalid" do
+    assert %{
+             name: "Mat",
+             email: "matheusmailcom",
+             base_salary: 1200,
+             office: :dev
+           }
+           |> Salario.calc() == :informations_invalid
+  end
+
+  test "user have base salary invalid" do
+    assert %{
+             name: "Mat",
+             email: "matheus@mail.com",
+             base_salary: -1,
+             office: :dev
+           }
+           |> Salario.calc() == :informations_invalid
+  end
+
+  test "user have office invalid" do
+    assert %{
+             name: "Mat",
+             email: "matheus@mail.com",
+             base_salary: 1200,
+             office: :d
+           }
+           |> Salario.calc() == :informations_invalid
+  end
+
+  test "dev with salary smaller then 3.000" do
+    assert %{
+             name: "Mat",
+             email: "matheus@mail.com",
+             base_salary: 2999.99,
+             office: :dev
+           }
+           |> Salario.calc() == 2699.99
+  end
+
+  test "dev with salary bigger then 3.000" do
+    assert %{
+             name: "Mat",
+             email: "matheus@mail.com",
+             base_salary: 3000,
+             office: :dev
+           }
+           |> Salario.calc() == 2400
+  end
+
+  test "dba with salary smaller then 2.000" do
+    assert %{
+             name: "Mat",
+             email: "matheus@mail.com",
+             base_salary: 1999.99,
+             office: :dba
+           }
+           |> Salario.calc() == 1699.99
+  end
+
+  test "dba with salary bigger then 2.000" do
+    assert %{
+             name: "Mat",
+             email: "matheus@mail.com",
+             base_salary: 2000,
+             office: :dba
+           }
+           |> Salario.calc() == 1500
+  end
+
+  test "tst with salary smaller then 2.000" do
+    assert %{
+             name: "Mat",
+             email: "matheus@mail.com",
+             base_salary: 1999.99,
+             office: :tst
+           }
+           |> Salario.calc() == 1699.99
+  end
+
+  test "tst with salary bigger then 2.000" do
+    assert %{
+             name: "Mat",
+             email: "matheus@mail.com",
+             base_salary: 2000,
+             office: :tst
+           }
+           |> Salario.calc() == 1500
+  end
+
+  test "ger with salary smaller then 5.000" do
+    assert %{
+             name: "Mat",
+             email: "matheus@mail.com",
+             base_salary: 4999.99,
+             office: :ger
+           }
+           |> Salario.calc() == 3999.99
+  end
+
+  test "ger with salary bigger then 5.000" do
+    assert %{
+             name: "Mat",
+             email: "matheus@mail.com",
+             base_salary: 5000,
+             office: :ger
+           }
+           |> Salario.calc() == 3500
+  end
+end
